262ab1696471a5a8dea209568380090969a9bcec
Houssem <133882779+houssemnaff@users.noreply.github.com>
2025-05-24 23:03:15 +0200

    v2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getUserById, deleteUserById, updateUserById, getUserByIdparms } = require("../controllers/userController");
+const { getUserById, deleteUserById, updateUserById, getUserByIdparms } = require("../controllers/usercontroller");
 const { protect } = require("../middleware/authMiddleware");
 const uploadMiddleware = require("../utils/multerConfig");
 
